15417fe6f6a8f025be6b2c888bd945ea3ef6c61a
Vaidas <valikonis.vaidas@gmail.com>
2024-09-21 13:18:02 +0200

    Initial commit

---
+++ links/src/test/java/dev/links/LinksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.links;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
