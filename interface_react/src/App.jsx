db8b1f71d6ca969bad87169b698bd8771e05515d
Vaidas <valikonis.vaidas@gmail.com>
2024-09-21 15:56:01 +0200

    Main page interface update

--- interface_react/src/App.jsx
+++ interface_react/src/App.jsx
@@ -30,20 +30,55 @@ function App() {
         <React.Fragment>
             <Box sx={{width: '100%', typography: 'body1'}}>
                 <TabContext value={value}>
-                    <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-                        <TabList onChange={handleChange} aria-label="lab label API tabs example"
+                    <Box sx={{
+                        borderBottom: 1, borderColor: 'rgba(103,163,163,0.1)'
+                    }}>
+                        <TabList onChange={handleChange}
+                                 textColor="white"
+                                 indicatorColor="white"
+                                 aria-label="lab label API tabs example"
                                  centered
-                                 inkBarStyle={{background: 'blue'}}
                                  sx={{
-                                     backgroundColor: "#f0f0f0", // Change to your desired color
-                                 }}>
-                            <Tab icon={<DashboardIcon/>} aria-label="dashboard" value="dashboard"/>
-                            <Tab icon={<LanguageIcon/>} aria-label="links" value="links"/>
-                            <Tab icon={<ListAltIcon/>} aria-label="todos" value="todos"/>
-                            <Tab icon={<EventNote/>} aria-label="notes" value="notes"/>
-                            <Tab icon={<Feed/>} aria-label="news" value="news"/>
-                            <Tab icon={<WbSunny/>} aria-label="weather" value="weather"/>
-                            <Tab icon={<CalendarMonth/>} aria-label="calendar" value="calendar"/>
+                                     backgroundColor: 'transparent'
+                                 }}
+                        >
+                            <Tab icon={<DashboardIcon/>} aria-label="dashboard" value="dashboard"
+                                 sx={{
+                                     padding: 0,
+                                     margin: 0,
+                                     backgroundColor: value === "dashboard" ? "rgb(103,163,163)" : "transparent",
+                                     color: value === "dashboard" ? "white" : "rgb(103,163,163)"
+                                 }}/>
+                            <Tab icon={<LanguageIcon/>} aria-label="links" value="links"
+                                 sx={{
+                                     backgroundColor: value === "links" ? "rgb(103,163,163)" : "transparent",
+                                     color: value === "links" ? "white" : "rgb(103,163,163)"
+                                 }}/>
+                            <Tab icon={<ListAltIcon/>} aria-label="todos" value="todos"
+                                 sx={{
+                                     backgroundColor: value === "todos" ? "rgb(103,163,163)" : "transparent",
+                                     color: value === "todos" ? "white" : "rgb(103,163,163)"
+                                 }}/>
+                            <Tab icon={<EventNote/>} aria-label="notes" value="notes"
+                                 sx={{
+                                     backgroundColor: value === "notes" ? "rgb(103,163,163)" : "transparent",
+                                     color: value === "notes" ? "white" : "rgb(103,163,163)"
+                                 }}/>
+                            <Tab icon={<Feed/>} aria-label="news" value="news"
+                                 sx={{
+                                     backgroundColor: value === "news" ? "rgb(103,163,163)" : "transparent",
+                                     color: value === "news" ? "white" : "rgb(103,163,163)"
+                                 }}/>
+                            <Tab icon={<WbSunny/>} aria-label="weather" value="weather"
+                                 sx={{
+                                     backgroundColor: value === "weather" ? "rgb(103,163,163)" : "transparent",
+                                     color: value === "weather" ? "white" : "rgb(103,163,163)"
+                                 }}/>
+                            <Tab icon={<CalendarMonth/>} aria-label="calendar" value="calendar"
+                                 sx={{
+                                     backgroundColor: value === "calendar" ? "rgb(103,163,163)" : "transparent",
+                                     color: value === "calendar" ? "white" : "rgb(103,163,163)"
+                                 }}/>
                         </TabList>
                     </Box>
                     <TabPanel value="dashboard"><Main/></TabPanel>
@@ -53,12 +88,8 @@ function App() {
                     <TabPanel value="news"><News/></TabPanel>
                     <TabPanel value="weather"><Weather/></TabPanel>
                     <TabPanel value="calendar"><Calendar/></TabPanel>
-
                 </TabContext>
-
             </Box>
-
-
         </React.Fragment>
     );
 }
--- interface_react/src/components/Main.jsx
+++ interface_react/src/components/Main.jsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import * as React from 'react';
+import styles from './CSS/shared.module.css';
 
 export default function Main() {
 
     return (
-        <>
+        <div className={styles.content}>
             Main Page
-        </>
+        </div>
     );
 }
