508a0c4210a338152e2a2bf34d7781490b7ea904
Vaidas <valikonis.vaidas@gmail.com>
2024-09-21 14:57:13 +0200

    Added Material UI with tabs to interface

--- interface_react/src/App.jsx
+++ interface_react/src/App.jsx
@@ -1,13 +1,60 @@
 import './App.css'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import Tab from "@mui/material/Tab";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import LanguageIcon from "@mui/icons-material/Language";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import {CalendarMonth, EventNote, Feed, WbSunny} from "@mui/icons-material";
+import TabPanel from "@mui/lab/TabPanel";
+import Test from "./components/Test.jsx";
+
 
 function App() {
-  return (
-    <>
-      <div>
+    const [value, setValue] = React.useState('1');
 
-      </div>
-    </>
-  )
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    // const secondary = red[500]; // #f44336
+
+    return (
+        <React.Fragment>
+            <Box sx={{width: '100%', typography: 'body1'}}>
+                <TabContext value={value}>
+                    <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                        <TabList onChange={handleChange} aria-label="lab label API tabs example"
+                                 centered
+                                 inkBarStyle={{background: 'blue'}}
+                                 sx={{
+                                     backgroundColor: "#f0f0f0", // Change to your desired color
+                                 }}>
+                            <Tab icon={<DashboardIcon/>} aria-label="dashboard" value="dashboard"/>
+                            <Tab icon={<LanguageIcon/>} aria-label="links" value="links"/>
+                            <Tab icon={<ListAltIcon/>} aria-label="todos" value="todos"/>
+                            <Tab icon={<EventNote/>} aria-label="notes" value="notes"/>
+                            <Tab icon={<Feed/>} aria-label="news" value="news"/>
+                            <Tab icon={<WbSunny/>} aria-label="weather" value="weather"/>
+                            <Tab icon={<CalendarMonth/>} aria-label="calendar" value="calendar"/>
+                        </TabList>
+                    </Box>
+                    <TabPanel value="dashboard"><Test/></TabPanel>
+                    <TabPanel value="links"><Test/></TabPanel>
+                    <TabPanel value="todos"><Test/></TabPanel>
+                    <TabPanel value="notes"><Test/></TabPanel>
+                    <TabPanel value="news"><Test/></TabPanel>
+                    <TabPanel value="weather"><Test/></TabPanel>
+                    <TabPanel value="calendar"><Test/></TabPanel>
+
+                </TabContext>
+
+            </Box>
+
+
+        </React.Fragment>
+    );
 }
 
 export default App
---
+++ interface_react/src/components/Main.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import * as React from 'react';
+
+export default function Main() {
+
+    return (
+        <></>
+    );
+}
